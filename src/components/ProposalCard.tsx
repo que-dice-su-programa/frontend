5a2a67760b06cc6b3e477399fe1538bd2129562a
uesteibar <uesteibar@gmail.com>
2023-07-07 03:18:59 +0200

    Replace logo with color underline

--- src/components/ProposalCard.tsx
+++ src/components/ProposalCard.tsx
@@ -13,20 +13,6 @@ import {
 } from "@chakra-ui/react";
 import { ArrowUpIcon } from "@chakra-ui/icons";
 
-const politicalSpectre = {
-  pp: "Centro-derecha",
-  psoe: "Centro-izquierda",
-  sumar: "Izquierda",
-  vox: "Derecha",
-  bildu: "Izquierda",
-  pnv: "Centro-derecha",
-  erc: "Izquierda",
-  junts: "Derecha",
-  upn: "Centro-derecha",
-  bng: "Izquierda",
-  cc: "Centro",
-};
-
 const colors = {
   pp: "#16589d",
   psoe: "#f31a11",
@@ -38,17 +24,9 @@ export const ProposalCard = ({ party, content }) => {
   const logoPath = `/logos/${party}.png`;
   return (
     <Card maxW="md" height={375} key={party}>
-      <Box height={2} backgroundColor={colors[party]} />
       <CardHeader>
-        <Flex flex="4">
-          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-            <Avatar name={party} src={logoPath} objectFit="contain" />
-            <Box>
-              <Heading size="sm">{party.toUpperCase()}</Heading>
-              <Text>{politicalSpectre[party]}</Text>
-            </Box>
-          </Flex>
-        </Flex>
+        <Heading size="lg">{party.toUpperCase()}</Heading>
+      <Box height={2} backgroundColor={colors[party]} />
       </CardHeader>
       <CardBody>
         <Text>{content.result}</Text>
