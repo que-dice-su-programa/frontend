6b73883109484f495b31a10a38a46923059effca
uesteibar <uesteibar@gmail.com>
2023-07-07 02:49:57 +0200

    Rotating input placeholder

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from 'react';
 import {
   Flex,
   Text,
@@ -10,7 +11,41 @@ import {
 } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 
+const shuffle = (array: string[]) => {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+const proposals = shuffle([
+  "la vivienda",
+  "la violencia machista",
+  "el feminismo",
+  "la justicia fiscal",
+  "los impuestos",
+  "el sistema sanitario"
+])
+
 export const SearchBox = ({ searchField, setSearchField, handleSubmit }) => {
+  const [proposalIndex, setProposalIndex] = useState(0);
+  const proposal = proposals[proposalIndex]
+
+  setTimeout(() => {
+    proposalIndex < proposals.length - 1 ? setProposalIndex(proposalIndex+1) : setProposalIndex(0)
+  }, 4000)
+
   return (
     <Flex
       justifyContent="center"
@@ -31,7 +66,7 @@ export const SearchBox = ({ searchField, setSearchField, handleSubmit }) => {
       >
         <InputGroup>
           <Input
-            placeholder="Qué dicen los partidos sobre..."
+            placeholder={proposal}
             height={55}
             value={searchField}
             onChange={(e) => setSearchField(e.target.value)}
